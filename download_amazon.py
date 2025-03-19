8dbcddf00c10c5de411df276279b84302adc7cf2
Noah682 <noahwei682@gmail.com>
2025-03-19 20:22:33 +0800

    Update download_amazon.py

--- download_amazon.py
+++ download_amazon.py
@@ -5,7 +5,7 @@ import json
 
 data = load_dataset("wei682/Cambrian-LLaVA-Amazon-MLLM-objects",'All_Beauty', split="train")
 
-image_folder = "./mydatasets/llava_onevision/images/FigureQA_MathV360K/"
+image_folder = "./mydatasets/amazon/images/all_beauty/"
 
 converted_data = []
 
@@ -13,11 +13,11 @@ for da in tqdm(data):
     json_data = {}
     json_data["id"] = da["id"]
     if da["image"] is not None:
-        json_data["image"] = f"{da['id']}.rgba"
-        da["image"].save(os.path.join(image_folder, json_data["image"]))
+        json_data["image"] = f"{da['id']}.jpg"
+        da["image"].save(os.path.join(image_folder, json_data["image"])) 
     json_data["conversations"] = da["conversations"]
     converted_data.append(json_data)
 
 
-with open("./mydatasets/llava_onevision/llava_onevision_FigureQA_MathV360K.json", "w") as f:
+with open("./mydatasets/amazon/amazon_all_beauty_qa_it_data.json", "w") as f:
     json.dump(converted_data, f, indent=4, ensure_ascii=False)
